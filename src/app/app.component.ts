eb5363b4c981390563adc3ba32bbd668800ab969
rajeshm101 <rajesh.moorjani@gmail.com>
2019-05-20 17:48:08 +0530

    Update app.component.ts
    app changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MarvelService } from './services/marvel.service';
 import { Comic } from './models/comic.model';
@@ -11,33 +12,56 @@ import { MarvelResponse } from './models/marvel.model';
   ]
 })
 export class AppComponent implements OnInit {
-  title: string = "Search your Marvel Comics";
+  title: string = "Search your Marvel Comics2ewr";
   attribution : string;
   isLoading: boolean = false;
   comics : Comic[] = [];
   selectedComic: Comic;
   shown : number = 20;
-  year : number = 0;
+  year : number = 2018;
   total : number = null;
-  filter : string = "spider";
+  filter : string = "";
+  lastFilteredItem: string = "";
   @ViewChild('staticModal') staticModal;
 
   constructor(private _marvelService : MarvelService) {}
   async ngOnInit() {
-    await this.refreshList();
-  }
-  async refreshList() {
-    this.isLoading = true
-    let response : MarvelResponse<Comic> = await this._marvelService.getComics(this.shown, this.filter, this. year);
+     this.isLoading = true;
+     this.lastFilteredItem = this.filter;
+       let response : MarvelResponse<Comic> = await this._marvelService.getComics(this.shown, this.filter, this. year);
     this.comics = response.data.results.map(item => {
       item.thumbnail.path = item.thumbnail.path.replace('http', 'https');
       item.saleDate = item['dates'].find(dateItem => dateItem.type === 'onsaleDate')
       item.price = item['prices'].find(price => price.type === 'printPrice')
       return item;
     });
+      this.isLoading = false;
+      this.total = response.data.total;
+    this.attribution = response.attributionHTML;
+  }
+  async refreshList(doForcefully:boolean) {
+       if((this.lastFilteredItem == this.filter) && !doForcefully){
+      //do nothing already fetched
+      console.log("already filtered");
+      return;
+    }
+
+    //Hint: Uncomment the below section to fix the search box!"
+    
+    this.isLoading = true;
+    this.lastFilteredItem = this.filter;
+       let response : MarvelResponse<Comic> = await this._marvelService.getComics(this.shown, this.filter, this. year);
+    this.comics = response.data.results.map(item => {
+      item.thumbnail.path = item.thumbnail.path.replace('http', 'https');
+      item.saleDate = item['dates'].find(dateItem => dateItem.type === 'onsaleDate')
+      item.price = item['prices'].find(price => price.type === 'printPrice')
+      return item;
+    });
+  
     this.total = response.data.total;
     this.attribution = response.attributionHTML;
     this.isLoading = false
+     
   }
   openModal(index) {
     this.selectedComic = this.comics[index]
